93aa17ada3ac812268b4dda80a8d0e81e0fdf29f
Michael Schennum <mikeschenphoto@gmail.com>
2016-08-09 11:48:33 -0700

    adds blog

--- navbar.php
+++ navbar.php
@@ -10,18 +10,19 @@
       <a class="navbar-brand" href="/"><span id="header">Michael Schennum</span></a>
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-left">
-        <li><a href="aboutme.php">About</a></li>
+        <li><a href="aboutme.php"><font size="2">About Me</font></a></li>
+        <li><a href="blog"><font size="2">Blog</font></a></li>
       </ul>
        <!--  <a class="navbar-brand" href="https://still-lowlands-91555.herokuapp.com">Ask Me App</a>
         <a class="navbar-brand" href="https://safe-badlands-82847.herokuapp.com">Dictionary App</a>
         <a class="navbar-brand" href="index1.php">Professional Photography Website</a> -->
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="https://github.com/mikeschen" target="_blank"><img src="Images/Logos/github.png" /></a></li>
-        <li><a href="https://www.linkedin.com/in/mikeschen" target="_blank"><img src="Images/Logos/in.png" /></a>
-        <li><a href="https://twitter.com/MichaelSchennum" target="_blank"><img src="Images/Logos/twitter.png" /></a>
-        <li><a href="https://www.instagram.com/mikeschenphoto/" target="_blank"><img src="Images/Logos/instagram.png" /></a>
-        <li><a a href="https://www.facebook.com/michael.schennum" target="_blank"><img src="Images/Logos/fb.png" /></a>
-        <li><a href="mailto:mikeschenphoto@gmail.com"><img src="Images/Logos/email.png" /></a>
+        <li><a href="https://github.com/mikeschen" target="_blank"><i class="fa fa-github fa-2x"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/mikeschen" target="_blank"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a></li>
+        <li><a href="https://twitter.com/MichaelSchennum" target="_blank"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a></li>
+        <li><a href="https://www.instagram.com/mikeschenphoto/" target="_blank"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a></li>
+        <li><a a href="https://www.facebook.com/michael.schennum" target="_blank"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a></li>
+        <li><a href="mailto:mikeschenphoto@gmail.com"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a></li>
       </ul>
     </div>
   </div>
